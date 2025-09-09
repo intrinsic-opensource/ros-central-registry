ed5a9188b7e249185bbd1bd327d5d5830478425d
Andrew Symington <simmers@google.com>
2025-09-09 00:19:40 +0000

    Fix lttng-ust dependency

---
+++ thirdparty/lttng-ust/include/lttng/ust-config.h
@@ -0,0 +1,14 @@
+/* include/lttng/ust-config.h.  Generated from ust-config.h.in by configure.  */
+// SPDX-FileCopyrightText: 2023 EfficiOS, Inc
+//
+// SPDX-License-Identifier: MIT
+
+#ifndef _LTTNG_UST_CONFIG_H
+#define _LTTNG_UST_CONFIG_H
+
+/* lttng/ust-config.h.in. Manually generated for control over the contained defs. */
+
+/* DTrace/GDB/SystemTap integration via sdt.h */
+/* #undef LTTNG_UST_HAVE_SDT_INTEGRATION */
+
+#endif
