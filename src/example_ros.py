11f29c5031cd3a542a7f06e57495404d389b48bb
Andrew Symington <andrew.c.symington@gmail.com>
2025-12-16 07:28:28 -0800

    Work in progress support for rclpy
    Signed-off-by: Andrew Symington <andrew.c.symington@gmail.com>

--- src/example_ros.py
+++ src/example_ros.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+print(sys.path)
+
 from sensor_msgs.msg import CompressedImage
 
 if __name__ == '__main__':
