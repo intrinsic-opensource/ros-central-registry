981f7ce43d61e7962c5f46c9e849b7d7ae89125f
Andrew Symington <simmers@intrinsic.ai>
2026-03-17 00:04:01 +0000

    Added some skeleton vendoring code.
    Signed-off-by: Andrew Symington <simmers@intrinsic.ai>

--- tools/bazelflore/bazelflore/bazel/vendor_file.py
+++ tools/bazelflore/bazelflore/bazel/vendor_file.py
@@ -17,8 +17,9 @@ Generic module creator for the "ros" module.
 """
 
 from pathlib import Path
-from typing import Dict
-from bazelflore.bazel.constants import ROS_TO_BAZEL_MAPPING
+from typing import Dict, Set
+import re
+from bazelflore.bazel.constants import ROS_TO_BAZEL_MAPPING, get_copyright_header
 from bazelflore.bazel.module import Module
 from bazelflore.sources.bcr import BcrSource
 from bazelflore.sources.deb import DebSource
@@ -29,17 +30,43 @@ class VendorFile:
     def __init__(self, working_directory: Path):
         """Initialize the module."""
         self.module_dir = working_directory / 'modules'
+        self.bazelrc_file = working_directory / 'vendor' / '.bazelrc'
         self.vendor_file = working_directory / 'vendor' / 'VENDOR.bazel'
+
+    def _write_vendor_dot_bazel(self, bcr_deps : Set[str], rcr_deps : Set[str]):
+        ret = get_copyright_header()
+        for dep in sorted(bcr_deps):
+            ret += f'ignore("@@{dep}+")\n'
+        for dep in sorted(rcr_deps):
+            ret += f'pin("@@{dep}+")\n'
+        self.vendor_file.parent.mkdir(parents=True, exist_ok=True)
+        with open(self.vendor_file, 'w') as f:
+            f.write(ret)
+
+    def _write_dot_bazelrc(self, rcr_deps : Set[str]):
+        ret = get_copyright_header()
+        ret += "vendor --vendor_dir=vendor \\\n"
+        for rcr_dep in sorted(rcr_deps):
+            ret += f'--repo=@{rcr_dep} \\\n'
+        with open(self.bazelrc_file, 'w') as f:
+            f.write(ret)
 
     def generate_file(self):
         """Generate the module."""
 
-        ret = get_copyright_header()
-        # TODO(asymingt): scan the self.module_dir for bazel_dep() calls. If the
-        # name of the module is self.module_dir then pin this module. Otherwise,
-        # it must be a BCR module and we should ignore() ot.
+        deps = set()
+        for module_file in self.module_dir.rglob('MODULE.bazel'):
+            with open(module_file, 'r') as f:
+                content = f.read()
+                matches = re.findall(r'bazel_dep\(\s*name\s*=\s*"([^"]+)"', content)
+                deps.update(matches)
+        bcr_deps = set()
+        rcr_deps = set()
+        for dep in sorted(deps):
+            if (self.module_dir / dep).is_dir():
+                rcr_deps.add(dep)
+            else:
+                bcr_deps.add(dep)
 
-        # Write the vendor file.
-        self.vendor_file.parent.mkdir(parents=True, exist_ok=True)
-        with open(self.vendor_file, 'w') as f:
-            f.write(ret)
+        self._write_vendor_dot_bazel(bcr_deps, rcr_deps)
+        self._write_dot_bazelrc(rcr_deps)
--- tools/bazelflore/main.py
+++ tools/bazelflore/main.py
@@ -28,6 +28,7 @@ from bazelflore.bazel.constants import ROS_TO_BAZEL_MAPPING
 from bazelflore.bazel.package_module import PackageModule
 from bazelflore.bazel.ros_module import RosModule
 from bazelflore.bazel.rosdistro_module import RosdistroModule
+from bazelflore.bazel.vendor_file import VendorFile
 from bazelflore.sources.bcr import BcrWorker
 from bazelflore.sources.deb import DebWorker, COMPONENTS
 from bazelflore.sources.ros import RosWorker
@@ -72,77 +73,75 @@ def main():
     release_version = "{0}.{1}".format(args.ubuntu_distro, args.ros_release_date)
 
     with yaspin(text="Bootstrapping new release", color="cyan") as sp:
-        sp.write("> Setting up ubuntu worker")
-        deb_worker = DebWorker(args.working_directory, args.ubuntu_distro, args.ubuntu_architecture, args.ros_release_date)
-        deb_sources = {}
-        for component in COMPONENTS:
-            sp.write("> Fetching ubuntu component: {0}".format(component))
-            deb_sources.update(deb_worker.fetch_packages(component))
-        sp.write("> Collected info about {0} debians".format(len(deb_sources)))
+        # sp.write("> Setting up ubuntu worker")
+        # deb_worker = DebWorker(args.working_directory, args.ubuntu_distro, args.ubuntu_architecture, args.ros_release_date)
+        # deb_sources = {}
+        # for component in COMPONENTS:
+        #     sp.write("> Fetching ubuntu component: {0}".format(component))
+        #     deb_sources.update(deb_worker.fetch_packages(component))
+        # sp.write("> Collected info about {0} debians".format(len(deb_sources)))
 
-        sp.write("> Scanning BCR for latest module versions")
-        bcr_worker = BcrWorker(args.working_directory)
-        bcr_sources = bcr_worker.fetch_packages()
-        sp.write("> Collected info about {0} modules".format(len(bcr_sources)))
+        # sp.write("> Scanning BCR for latest module versions")
+        # bcr_worker = BcrWorker(args.working_directory)
+        # bcr_sources = bcr_worker.fetch_packages()
+        # sp.write("> Collected info about {0} modules".format(len(bcr_sources)))
 
-        sp.write("> Setting up rosdistro snapshot")
-        ros_worker = RosWorker(args.working_directory, args.ros_release_distro, args.ros_release_date)
-        ros_sources = ros_worker.fetch_packages()
-        sp.write("> Collected info about {0} packages".format(len(ros_sources)))
+        # sp.write("> Setting up rosdistro snapshot")
+        # ros_worker = RosWorker(args.working_directory, args.ros_release_distro, args.ros_release_date)
+        # ros_sources = ros_worker.fetch_packages()
+        # sp.write("> Collected info about {0} packages".format(len(ros_sources)))
 
-        sp.write("> Creating Bazel modules for all ROS packages")
-        for pkg_name in sorted(ros_sources.keys()):
-            # We intentionally choose to skip some vendored packages in the BCR:
-            if pkg_name in ROS_TO_BAZEL_MAPPING and ROS_TO_BAZEL_MAPPING[pkg_name] is None:
-                sp.write("> Skipping vendored package {0}".format(pkg_name))
-                continue
-            # If we get here, we need to generate a package module for the current package.
-            pkg_info = ros_sources[pkg_name]
-            sp.write("> Processing {0}".format(pkg_name))
-            package_module = PackageModule(
-                working_directory=args.working_directory,
-                bcr_sources=bcr_sources,
-                deb_sources=deb_sources,
-                ros_sources=ros_sources,
-                release_distro=args.ros_release_distro,
-                release_date=args.ros_release_date,
-                module_name=pkg_name,
-                module_version=pkg_info.version,
-                module_url=pkg_info.url,
-            )
-            for dep in pkg_info.dependencies:
-                package_module.add_dependency(dep)
-            package_module.generate_module()
-        sp.write("> Created package bazel modules")
+        # sp.write("> Creating Bazel modules for all ROS packages")
+        # for pkg_name in sorted(ros_sources.keys()):
+        #     # We intentionally choose to skip some vendored packages in the BCR:
+        #     if pkg_name in ROS_TO_BAZEL_MAPPING and ROS_TO_BAZEL_MAPPING[pkg_name] is None:
+        #         sp.write("> Skipping vendored package {0}".format(pkg_name))
+        #         continue
+        #     # If we get here, we need to generate a package module for the current package.
+        #     pkg_info = ros_sources[pkg_name]
+        #     sp.write("> Processing {0}".format(pkg_name))
+        #     package_module = PackageModule(
+        #         working_directory=args.working_directory,
+        #         bcr_sources=bcr_sources,
+        #         deb_sources=deb_sources,
+        #         ros_sources=ros_sources,
+        #         release_distro=args.ros_release_distro,
+        #         release_date=args.ros_release_date,
+        #         module_name=pkg_name,
+        #         module_version=pkg_info.version,
+        #         module_url=pkg_info.url,
+        #     )
+        #     for dep in pkg_info.dependencies:
+        #         package_module.add_dependency(dep)
+        #     package_module.generate_module()
+        # sp.write("> Created package bazel modules")
 
-        sp.write("> Creating rosdistro bazel module")
-        rosdistro_module = RosdistroModule(
-            working_directory=args.working_directory,
-            bcr_sources=bcr_sources,
-            deb_sources=deb_sources,
-            ros_sources=ros_sources,
-            release_distro=args.ros_release_distro,
-            release_date=args.ros_release_date,
-        )
-        rosdistro_module.generate_module()
-        sp.write("> Created rosdistro bazel module")
+        # sp.write("> Creating rosdistro bazel module")
+        # rosdistro_module = RosdistroModule(
+        #     working_directory=args.working_directory,
+        #     bcr_sources=bcr_sources,
+        #     deb_sources=deb_sources,
+        #     ros_sources=ros_sources,
+        #     release_distro=args.ros_release_distro,
+        #     release_date=args.ros_release_date,
+        # )
+        # rosdistro_module.generate_module()
+        # sp.write("> Created rosdistro bazel module")
     
-        sp.write("> Creating ros bazel module")
-        ros_module = RosModule(
-            working_directory=args.working_directory,
-            bcr_sources=bcr_sources,
-            deb_sources=deb_sources,
-            ros_sources=ros_sources,
-            release_distro=args.ros_release_distro,
-            release_date=args.ros_release_date,
-        )
-        ros_module.generate_module()
-        sp.write("> Created ros bazel module")
+        # sp.write("> Creating ros bazel module")
+        # ros_module = RosModule(
+        #     working_directory=args.working_directory,
+        #     bcr_sources=bcr_sources,
+        #     deb_sources=deb_sources,
+        #     ros_sources=ros_sources,
+        #     release_distro=args.ros_release_distro,
+        #     release_date=args.ros_release_date,
+        # )
+        # ros_module.generate_module()
+        # sp.write("> Created ros bazel module")
 
         sp.write("> Creating vendor file")
-        vendor_file = VendorFile(
-            working_directory=args.working_directory,
-        )
+        vendor_file = VendorFile(working_directory=args.working_directory)
         vendor_file.generate_file()
         sp.write("> Created vendor file")
 
